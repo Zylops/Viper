b5d4e29b7198f5fd4d97c8d4abcd75c849814baa
Zylops <zainaazeez787@gmail.com>
2021-07-28 19:47:34 +0300

    Updated Example

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Viper!
-A python, object oriented library to generate web pages with just python!
+A python, object oriented library to generate web pages with just python! Viper uses Jinja2 as a backend to render source code for every instance of any module.
 
 ![Viper Banner](https://imgur.com/5JwgpaU.png)
 
--- example.py
+++ example.py
@@ -10,11 +10,13 @@ def home():
     about = Paragraph('This is an example page with all available modules used. This page will be updated for new modules (if any). Thanks for checking it out! Have fun modulating!')
     gitlink = Link(url='https://github.com/Zylops/Viper', text='View on GitHub!')
     developers = Table(headers=['Authors'], rows=['Zylops', '0xPiyush'])
+    
     header = Div(id='header', styles=['padding:20px;', 'text-align:center;', 'height:100%;'], modules=[logo, LineBreak(), heading, HorizontalLine()])
     content = Div(id='content', styles=['padding:5px;', 'text-align:center;', 'height:100%;'], modules=[about, gitlink, developers])
-    body = Body(modules=[header, content])
-    page = Page(title='Viper!', body=body, stylesheets=['https://www.w3schools.com/w3css/4/w3.css', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css'])
-    print(page.source())
+    stylesheets = ['https://www.w3schools.com/w3css/4/w3.css', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css']
+    
+    page = Page(title='Viper!', body=Body(modules=[header, content]), stylesheets=stylesheets)
+    
     return page.source()
 
 app.run(debug=True)
